571a54c9944eaa3f24ae1e50b5e0ae3c65c7df6b
Hernan Rocha <2688328+hernanrocha@users.noreply.github.com>
2019-09-17 16:22:46 -0300

    Add CircleCI Integration (#2)
    * Add makefile, dockerfile and circleci config
    
    * Add deploy stage
    
    * Fix variables
    
    * Deploy to S3
    
    * Update config.yml
    
    * Update pipeline
    
    * Update pipeline

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ Vue.config.productionTip = false;
 
 Vue.use(VueChatScroll)
 Vue.prototype.$http = axios;
-axios.defaults.baseURL = "http://localhost:8001";
+axios.defaults.baseURL = process.env.API_URL || "http://localhost:8001";
 
 Vue.filter("timestamp", value => {
   return moment(value).format("LLL");
